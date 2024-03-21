25c9f43af2722bb525d98313bd90d244f8aba835
Julia Loftin <julia.loftin7@gmail.com>
2024-03-20 21:27:11 -0500

    grabs user data

---
+++ src/components/CreateAPlaylist.tsx
@@ -0,0 +1,14 @@
+
+const CreateAPlaylist = () => {
+
+    return (
+        <div>
+            <form>
+                <input type="text" name="playlistName" placeholder="Playlist Name" />
+                <input type="text" name="playlistDescription" placeholder="Playlist Description" />
+            </form>
+        </div>
+    )
+}
+
+export default CreateAPlaylist;
