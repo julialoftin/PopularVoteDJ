83d71455b68f7451fb0e72e0a7ab4bf9500ccd3c
Julia Loftin <julia.loftin7@gmail.com>
2024-03-20 20:28:15 -0500

    corrects spotify auth flow

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,18 @@ import "./App.css";
 import SpotifyAuth from "./components/InitiateSpotifyAuth";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Admin from "./pages/Admin";
-import PrivateRoutes from "./components/PrivateRoutes";
 import Home from "./pages/Home";
-import Navbar from "./components/Navbar";
+import { useEffect, useState } from "react";
 
 function App() {
+
   return (
     <>
       <Router>
-        <Navbar />
         <Routes>
-          <Route element={<PrivateRoutes />}>
-            <Route path="/admin" element={<Admin />} />
-          </Route>
           <Route path="/" element={<Home />} />
           <Route path="/authorize" element={<SpotifyAuth />} />
+          <Route path="/admin" element={<Admin />} />
         </Routes>
       </Router>
     </>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,6 +5,7 @@ export default function Home () {
 
     return (
         <>
+            <Link to='/authorize'><button>Log in With Spotify!</button></Link>
             <h1>PopularVoteDJ</h1>
         </>
     )
