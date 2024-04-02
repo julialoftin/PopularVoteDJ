11bb6390c4728ad33c563b97fdac14fac8aaabf3
Julia Loftin <julia.loftin7@gmail.com>
2024-04-02 18:16:21 -0500

    server runs in docker at host 8080

--- webapp/vite.config.ts
+++ webapp/vite.config.ts
@@ -3,13 +3,16 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/",
   plugins: [react()],
-  server: {
-    watch: {
-      usePolling: true,
-    },
-    host: true, // needed for the Docker Container port mapping to work
-    strictPort: true,
-    port: 5173, // you can replace this port with any port
+  preview: {
+   port: 8080,
+   strictPort: true,
   },
-});
+  server: {
+   port: 8080,
+   strictPort: true,
+   host: true,
+   origin: "http://0.0.0.0:8080",
+  },
+ });
