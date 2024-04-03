461b3883cc9ec2e1f776070981384ee854c40df1
Julia Loftin <julia.loftin7@gmail.com>
2024-04-02 19:00:55 -0500

    changes npm run to dev, creates prod dockerfile

--- webapp/src/components/AdminNavbar.tsx
+++ webapp/src/components/AdminNavbar.tsx
@@ -1,10 +1,5 @@
 import { Link } from "react-router-dom";
 
-// interface props {
-//   setIsUserAuthenticated: React.Dispatch<React.SetStateAction<boolean | null>>;
-//   isUserAuthenticated: boolean | null;
-// }
-
 export default function AdminNavbar() {
   const handleLogout = () => {
     localStorage.removeItem("access_token");
