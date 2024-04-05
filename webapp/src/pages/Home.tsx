aea2b5a3b16398fc986846fa13a854276e3bee65
Julia Loftin <julia.loftin7@gmail.com>
2024-04-04 19:10:20 -0500

    adds home page statements about event codes and hosts

--- webapp/src/pages/Home.tsx
+++ webapp/src/pages/Home.tsx
@@ -1,12 +1,12 @@
-import LandingNavbar from "../components/LandingNavbar"
+import LandingNavbar from "../components/LandingNavbar";
 
-export default function Home () {
-
-
-    return (
-        <>
-            <LandingNavbar />
-            <h1>PopularVoteDJ</h1>
-        </>
-    )
+export default function Home() {
+  return (
+    <>
+      <LandingNavbar />
+      <h1>PopularVoteDJ</h1>
+      <p>Hosting an event? Log in with spotify!</p>
+      <p>Attending an event? Enter the Event Code here!</p>
+    </>
+  );
 }
